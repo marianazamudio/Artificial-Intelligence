04855949326f21af9fad0a084b7b4bb9448d9efc
Mariana Zamudio Ayala <120000587+marianazamudio@users.noreply.github.com>
2023-05-17 18:07:49 -0700

    New program for Assigment 6.
    Programs that creates training points in two regions in moon shape. These are defined by vertical distance between them, radious, and width. Currently coords in region A are obtained succesfully. For region B, is stuck in an infinite loop

---
+++ lunas/rnd_coord.py
@@ -0,0 +1,52 @@
+import random
+import math
+import matplotlib.pyplot as plt
+
+def generate_coords_in_moon(r, w, d, num_coord, region='A'):
+    # Obtain limits in of regions in axis x and y
+    # Limits form rectangular zones
+    if region =='A':
+        limits_x = (-r-w/2, r+w/2)
+        limits_y = (0, r+w/2)
+        center = (0,0)
+    elif region == 'B':
+        limits_x = (-w/2, 2*r+w/2)
+        limits_y = (-d, -d-r-w/2)
+        center = (r,-d)
+    
+    counter = 0
+    coords_x = []
+    coords_y = []
+    print(counter, num_coord)
+    
+    while counter < num_coord:
+        x = random.uniform(limits_x[0], limits_y[1])  # Generate a random value for x
+        y = random.uniform(limits_y[0], limits_y[1])  # Generate a random value for y
+
+        # ---Verify if the point is inside the moon
+        # Obtain distance between center and the rdm point
+        distance = math.sqrt((x-center[0])**2 + (y-center[1])**2)
+        # Based on the distance verify if the rdm point is inside the moon
+        if (r-w/2 <= distance <= r+w/2):
+            coords_x.append(x)
+            coords_y.append(y)
+            counter += 1
+        print(coords_x)
+    return (coords_x, coords_y)
+
+# --------------Test
+r = 3  # Radio interior de la luna menor
+d = 2  # Distancia vertical entre los centros de las lunas
+w = 1  # Anchura de cada luna
+
+# Obtener coordenadas en region A
+#coords_xA, coords_yA = generate_coords_in_moon(r, w, d, 500)
+
+# Obtener coordenadas en region B
+coords_xB, coords_yB = generate_coords_in_moon(r,w,d,500, "B")
+
+# Graficar coordenadas
+#plt.scatter(coords_xA, coords_yA)
+plt.scatter(coords_xB, coords_yB, marker="x")
+plt.grid(True)
+plt.show()

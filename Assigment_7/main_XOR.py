0339725050beedcbdf800da1927c0997d4766728
marianazamudio <120000587+marianazamudio@users.noreply.github.com>
2023-06-04 10:56:19 -0700

    deleted best MSE and fixed MSE plot

--- Assigment_7/main_XOR.py
+++ Assigment_7/main_XOR.py
@@ -10,7 +10,8 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-max_epochs = 1000
+# Tunning parameters
+max_epochs = 550
 alpha = 0.01
 a = 2
 eta = 0.5
@@ -22,6 +23,7 @@ num_neurons = [2,1]
 per_mult_layer = InterconNeuralNet(inputs, num_layers, num_neurons, 2, a)
 print(per_mult_layer.weights)
 
+# Weight initialization for convergence
 #capa_1 = np.array([[-1.5, 1, 1],[-0.5, 1, 1]])
 #capa_2 = np.array([[-0.5, -2, 1]])
 #per_mult_layer.weights = [capa_1, capa_2]
@@ -32,62 +34,50 @@ d =[1,0,0,1]
 
 # List to plot MSE
 MSE_list = []
-# Store best results
-best_MSE = 1000
-best_weights = []
 
+# Iterate between epochs of training
 for i in range(max_epochs):
     # Intialize list with idx for data set
     idx_list = list((range(4)))
-    print(idx_list, "idx_list")
+    
     # Permutate list
     random.shuffle(idx_list)
-    #print(idx_list, "idx_list")
+
+    # Initialize MSE result variable
+    MSE = 0
 
     # Iterate in data set
-    o = []
-    
     for idx in idx_list:
         # Configurar entradas
-        print(data_set[idx], "DS-----------------------------------------")
         per_mult_layer.set_inputs(data_set[idx])
+        
         # Configurar valores deseados
         d_n = d[idx] 
-
+        
         # Forward computation
         o_n = per_mult_layer.compute_output()[-1]
         
         # Backward computation
         per_mult_layer.back_computation(eta=eta,alpha=alpha,d=d_n)
 
-        o.append(o_n)
+        MSE += (d_n - o_n)**2
     
     # Compute MSE
-    MSE = (np.array(d) - np.array(o))
-    MSE = np.square(MSE)
-    MSE = np.sum(MSE)/(len(d))
+    MSE = MSE/(len(d))
     print(MSE, "mse")
     MSE_list.append(MSE)
-
-    if MSE < best_MSE:
-        best_MSE = MSE
-        best_weights = per_mult_layer.weights
+    
+    # Debug prints
     #print(o, "o")
     #print(d,"d")
     #input()
 
-    # break condition 
+    # Break condition 
     if MSE < 0.005:
         break
 
 print(f"termino en {i} epocas")
 
-# Graficar MSE
-x = np.arange(len(MSE_list))
-plt.plot(x, MSE_list)
-
-
-#input()
 # TEST last weights
 print("XOR OPERATION ---- last data")
 print("----------------------------")
@@ -98,16 +88,8 @@ for (x_n,d_n) in zip(data_set, d):
     o_n = per_mult_layer.compute_output()[-1]
     print(x_n, "\t  ",d_n, "   ",o_n)
 
-# TEST best weights
-if MSE != best_MSE:
-    per_mult_layer.weights = best_weights
-    print("\nXOR OPERATION ---- best MSE")
-    print("----------------------------")
-    print("Entradas   Exp.Res   Obt.Res")
-    print("----------------------------")
-    for (x_n,d_n) in zip(data_set, d):
-        per_mult_layer.set_inputs(x_n)
-        o_n = per_mult_layer.compute_output()[-1]
-        print(x_n, "\t  ",d_n, "   ",o_n)
-
+# Graficar MSE
+x = np.arange(len(MSE_list))
+plt.plot(x, MSE_list)
+# Show plot
 plt.show()
